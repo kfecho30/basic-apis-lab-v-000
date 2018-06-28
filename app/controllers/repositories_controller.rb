d791642b9e01c6e3a3aa3a58c00b980ad1eb6630
Kelsey Fecho <kfecho30@gmail.com>
2018-06-28 21:19:38 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @res = Faraday.get '' do |req|
+    @res = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = '0a45dd6c835edc4e28b1'
       req.params['client_secret'] = '30cd2d6589c1d57899f34520213b89824fd89cf8' 
     end
