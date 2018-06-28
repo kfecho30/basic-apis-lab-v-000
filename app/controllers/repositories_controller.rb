10260da4742c91c05412eac7f7d7c5dded05d95a
Kelsey Fecho <kfecho30@gmail.com>
2018-06-28 21:31:00 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -11,7 +11,7 @@ class RepositoriesController < ApplicationController
         req.params['client_secret'] = '30cd2d6589c1d57899f34520213b89824fd89cf8'
         req.params['q'] = params[:query]
       end
-
+      binding.pry
       items = JSON.parse(@res.items)
       if @res.success?
         @results = items
