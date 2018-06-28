6c47f4bb83e279da5148a6d8cc7bb8dd1398d808
Kelsey Fecho <kfecho30@gmail.com>
2018-06-28 21:29:37 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,17 +5,18 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
     @res = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = '0a45dd6c835edc4e28b1'
       req.params['client_secret'] = '30cd2d6589c1d57899f34520213b89824fd89cf8'
       req.params['q'] = params[:query]
     end
 
-    body = JSON.parse(@resp.body)
+    items = JSON.parse(@resp.items)
     if @res.success?
-      @results = body["response"]["venues"]
+      @results = items
     else
-      @error = body["meta"]["errorDetail"]
+      @error = items["meta"]["errorDetail"]
     end
 
   rescue Faraday::ConnectionFailed
